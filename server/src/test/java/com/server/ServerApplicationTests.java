7cdb2ada26a33b9437b1e4da2198640dd109e1a3
MingHaoC <chen1fl@uwindsor.ca>
2022-03-16 15:35:51 -0400

    Remove jpa from dependency until a sql server is up

--- server/src/test/java/com/server/ServerApplicationTests.java
+++ server/src/test/java/com/server/ServerApplicationTests.java
@@ -1,12 +1,8 @@
 package com.server;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ServerApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
