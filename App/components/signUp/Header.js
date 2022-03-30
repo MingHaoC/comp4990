326a9dcc3946d028732ae6ea22e0737fce1920c7
Ariana <avdouloa@uwindsor.ca>
2022-03-30 17:53:43 -0400

    Visuals for Login/SignUp page

---
+++ App/components/signUp/Header.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <View style={styles.headerView}>
+      <Text style={styles.headertitle}>Sign Up</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    headerView: {
+        width:'100%',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        marginVertical: 30,
+        paddingRight: 30
+    },
+
+    headertitle: {
+        fontSize: 30,
+        color: '#8A1005'
+    }
+})
