4e27ad2e575d3dab7dde2ca0d8704393c228dd0d
Ariana <avdouloa@uwindsor.ca>
2022-06-09 00:37:44 -0400

    Update App.js

--- App/App.js
+++ App/App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-import styles from './styles';
-import { AppProvider } from './context';
-import Login from './screens/Login';
-
-
-
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <AppProvider>
-    <StatusBar  />
     <View style={styles.container}>
-      <Login />
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
-    </AppProvider>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
