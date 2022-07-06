07c657ec1402b4b6c50e74aa937841d87ce45f86
nehpethP <sarkis8@uwindsor.ca>
2022-07-06 10:47:17 -0400

    Update scrape.js

--- App/scrape/scrape.js
+++ App/scrape/scrape.js
@@ -2,21 +2,8 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 
 const url = "https://windsorite.ca/events/categories/community/";
-// const baseUrl =
-//   "https://books.toscrape.com/catalogue/category/books/mystery_3/";
+
 const event_data = [];
-
-// async function getGenre() {
-//   try {
-//     const response = await axios.get(url);
-//     const $ = cheerio.load(response.data);
-//     const genre = $("h1").text();
-
-//     console.log(genre);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
 
 async function getEvents() {
   try {
@@ -32,11 +19,6 @@ async function getEvents() {
 
       event_data.push({ title, date, location });
     });
-
-    // if ($(".next a").length > 0) {
-    //   next_page = baseUrl + $(".next a").attr("href");
-    //   getBooks(next_page);
-    // }
 
     console.log(event_data);
   } catch (error) {
