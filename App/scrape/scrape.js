bc40b0c46338c13fcd46172fbc55e20db1ddf32b
nehpethP <sarkis8@uwindsor.ca>
2022-07-07 18:14:47 -0400

    updated scraping
    #9 small bug where saving the events to an array does not work

--- App/scrape/scrape.js
+++ App/scrape/scrape.js
@@ -1,29 +1,74 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-const url = "https://windsorite.ca/events/categories/community/";
+const baseURL = "https://www.eriestclairhealthline.ca/";
+const baseURLMain = baseURL + "listServices.aspx?id=10566&region=WindsorEssex";
 
-const event_data = [];
-
-async function getEvents() {
+async function getEventsList() {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(baseURLMain);
     const $ = cheerio.load(response.data);
+    const event_data = [];
 
-    const events = $(".right.rightener");
-    //console.log("Test " + $(":focus"));
-    events.each(function () {
-      var title = $(this).find("h2 a").text();
-      var date = $(this).find("div:first-of-type").text();
-      var location = $(this).find(".hidemobile").text();
+    const eventsURL = $(".serviceListing");
 
-      event_data.push({ title, date, location });
+    eventsURL.each(async function () {
+      var anchor = $(this).find("a");
+      if (anchor.attr("id").includes("RegionalServices")) {
+        var url = anchor.attr("href");
+        eventInfoURL = baseURL + url;
+
+        let event = await getEventInformation(eventInfoURL);
+        console.log(event);
+        //console.logging the above line works as intended, the issue arises when trying to push the event to the array, and then printing out the array afterwards
+        //the array prints out empty.
+        //maybe an issue with async await?
+
+        event_data.push(event);
+      }
     });
-
     console.log(event_data);
   } catch (error) {
     console.error(error);
   }
 }
 
-getEvents();
+async function getEventInformation(eventInfoURL) {
+  try {
+    const response2 = await axios.get(eventInfoURL);
+    const $ = cheerio.load(response2.data);
+
+    var title = $("#ctl00_ContentPlaceHolder1_lblProgram").text();
+    var phoneContact = $("#ctl00_ContentPlaceHolder1_lblOfficePhone").text();
+    var emailContact = $("#ctl00_ContentPlaceHolder1_lnkEmail").text();
+    var location = $("#ctl00_ContentPlaceHolder1_lblAddress").text();
+    var description = $("#ctl00_ContentPlaceHolder1_lblDescription").text();
+    var schedule = "";
+
+    //code to get the schedule of the events
+    var scheduleRows = $("#expandedhours > tbody > tr:nth-child(n+2)");
+
+    scheduleRows.each(function () {
+      schedule += $(this).find(".hours-label").text() + "@";
+      schedule += $(this).find("td:nth-child(2)").text() + " ";
+    });
+
+    if (!scheduleRows.length > 0) {
+      var schedule = $("#ctl00_ContentPlaceHolder1_lblHours").text();
+    }
+
+    return {
+      title,
+      description,
+      phoneContact,
+      emailContact,
+      location,
+      schedule,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getEventsList();
+//getEvents();
