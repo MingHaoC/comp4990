34d5d62f2e13376c4a887274e32803da8c4d7c19
Ming Hao Chen <chen1fl@uwindsor.ca>
2022-07-28 20:31:11 -0400

    fix scheduler missing code

--- server/src/main/java/com/server/component/NotificationScheduler.java
+++ server/src/main/java/com/server/component/NotificationScheduler.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.net.http.HttpResponse;
 import java.util.Calendar;
@@ -17,6 +18,7 @@ import static com.server.constant.URL.EXPO_SEND_NOTIFICATION_URL;
 import static com.server.util.HTTPRequest.sendNotification;
 import static java.util.Calendar.getInstance;
 
+@Component
 public class NotificationScheduler {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthenticationServiceImpl.class);
@@ -27,8 +29,9 @@ public class NotificationScheduler {
     @Autowired
     UserEventRepository userEventRepository;
 
-    @Scheduled(fixedRate = 600000)
+    @Scheduled(fixedRate = 30000)
     public void reportCurrentTime() {
+        System.out.println("test");
         Date now = new Date();
         Calendar calendar = getInstance();
         calendar.setTime(now);
